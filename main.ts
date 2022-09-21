3d328516895b3e4891ad5c937ea5bbdd8715591c
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-21 10:38:48 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    for (let index = 0; index <= 4; index++) {
+        for (let number = 0; number <= 4; number++) {
+            led.plot(number, index)
+            basic.pause(100)
+        }
+    }
 })
+basic.showIcon(IconNames.Angry)
+let number = 9
